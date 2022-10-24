5c91eba7ff0912ae5999aa5b474f352d1250b0ee
Tony Okeke <58089381+Kabilan108@users.noreply.github.com>
2022-10-24 01:00:37 -0400

    Add rough draft of GUI

---
+++ src/gui.py
@@ -0,0 +1,19 @@
+"""
+This script serves as the entry point for the GUI
+"""
+
+# Imports
+from PyQt5.QtWidgets import QApplication
+from gui_models import Camera
+from gui_views import StartWindow
+
+# Initialize camera
+camera = Camera(1)
+camera.initialize()
+
+
+# Launch app
+app = QApplication([])
+start_window = StartWindow(camera)
+start_window.show()
+app.exit(app.exec_())
