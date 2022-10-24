3aa454e4482be32899daf3c1ddc4ace1ae3cfc7e
Tony Okeke <58089381+Kabilan108@users.noreply.github.com>
2022-10-24 01:00:51 -0400

    Add webcam test

--- src/webcam-test.py
+++ src/webcam-test.py
@@ -1,2 +1,18 @@
 # Imports
 import cv2
+
+while(True):
+      
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
+  
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+      
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+  
